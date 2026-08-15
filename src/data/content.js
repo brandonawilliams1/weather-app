a542c9666c630c8f791e9ae298f85b11a73e7819
BRANDON WILLIAMS <31046919+brandonawilliams1@users.noreply.github.com>
2026-08-15 12:58:37 -0500

    Added led_truck.jpg

--- src/data/content.js
+++ src/data/content.js
@@ -6,8 +6,8 @@ export const IMAGES = {
     analytics: 'https://images.pexels.com/photos/12969403/pexels-photo-12969403.jpeg?auto=compress&cs=tinysrgb&w=800',
     creative: 'https://images.pexels.com/photos/13451104/pexels-photo-13451104.jpeg?auto=compress&cs=tinysrgb&w=800',
     truck1: 'assets/images/led_truck2.jpg',
-    truck2: 'assets/images/led_truck3.jpg',
-    truck3: 'assets/images/led_truck4.png
+    truck2: 'https://images.pexels.com/photos/13673160/pexels-photo-13673160.jpeg?auto=compress&cs=tinysrgb&w=800',
+    truck3: 'https://images.pexels.com/photos/756091/pexels-photo-756091.jpeg?auto=compress&cs=tinysrgb&w=800',
   },
 };
 

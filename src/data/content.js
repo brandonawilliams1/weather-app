103f075f27851dc3a9c776cc110db4120e5e3683
BRANDON WILLIAMS <31046919+brandonawilliams1@users.noreply.github.com>
2026-08-15 12:58:09 -0500

    Updated content.js

--- src/data/content.js
+++ src/data/content.js
@@ -7,7 +7,7 @@ export const IMAGES = {
     creative: 'https://images.pexels.com/photos/13451104/pexels-photo-13451104.jpeg?auto=compress&cs=tinysrgb&w=800',
     truck1: 'assets/images/led_truck2.jpg',
     truck2: 'assets/images/led_truck3.jpg',
-    truck3: 'assets/images/led_truck3.jpg',
+    truck3: 'assets/images/led_truck4.png
   },
 };
 
